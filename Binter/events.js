98be6412db12e902ba2f144a9afb67bc85d3e4f5
Alejandro <betancorta666@gmail.com>
2025-10-06 12:02:56 +0100

    boton funciona, pero mal, propuesta de mejora comentada.

--- Binter/events.js
+++ Binter/events.js
@@ -60,29 +60,40 @@ const asientos = {
             idAsiento.textContent = id;
             tipoAsiento.textContent = asientos.Turista.tipoAsiento;
             asientoDescription.textContent = asientos.Turista.asientoDescription;
-            precioAsiento.textContent = asientos.Turista.precioAsiento + "€";
+            precioAsiento.textContent += asientos.Turista.precioAsiento;
+            precioAsiento.classList.remove('hidden');
+            residnete.classList.remove('hidden');
             boton.classList.remove('hidden');
-/*boton.removeAttribute("id");
-            boton.setAttribute("id", "id_you_like");*/
-             // generarPago(id, asientos.Turista.tipoAsiento,  asientos.Turista.asientoDescription, asientos.Turista.precioAsiento, true)
-        }else if (type == "premium"){
+            }else if (type == "premium"){
             idAsiento.textContent = id;
             tipoAsiento.textContent = asientos.Premium.tipoAsiento;
             asientoDescription.textContent = asientos.Premium.asientoDescription;
-            precioAsiento.textContent = asientos.Premium.precioAsiento+ "€";
+            precioAsiento.textContent += asientos.Premium.precioAsiento;
+            precioAsiento.classList.remove('hidden');
+            residnete.classList.remove('hidden');
             boton.classList.remove('hidden');
             
         }else if (type == "ocupado"){
             idAsiento.textContent = id;
             tipoAsiento.textContent = asientos.Ocupado.tipoAsiento;
             asientoDescription.textContent = asientos.Ocupado.asientoDescription;
-            precioAsiento.textContent = asientos.Ocupado.precioAsiento+ "€";
+            precioAsiento.classList.add('hidden');
+            residnete.classList.add('hidden');
             boton.classList.add('hidden');
         }
         modal.classList.remove('hidden');
         
         boton.addEventListener('click', () => {
+          //Hay que implementar un localstorage para guardar todos estos datos y recuperarlos despues del href
+          //1ºguarda setContent key(data)   Info(idAsiento.textContent + tipoAsiento.textContent + asientoDescription.textContent + precioAsiento.textContent + residente.checked)
+          //2ºhref pantallaPago.html
+          //3º PantallaPago.html hace generarPantalla en js haciendo un get data 
+          //4º Al confirmar la compra hace un set idasiento --- ocupado
+          //5º todo funciona a la primera y soy felih, proyecto parte tecnica terminada, duplicar en iberia y ryanair :)!
     console.log('Hola' + idAsiento.textContent + tipoAsiento.textContent + asientoDescription.textContent + precioAsiento.textContent + residente.checked);
+    //location.href = "pantallaPago.html";
+    generarPago(idAsiento.textContent, tipoAsiento.textContent, asientoDescription.textContent, precioAsiento.textContent, residnete.checked);
+    
   });
     }
         
@@ -98,3 +109,17 @@ const asientos = {
   modal.addEventListener('click', (e) => {
     if (e.target === modal) modal.classList.add('hidden');
   });
+
+  function generarPago(idAsiento, tipoAsiento, asientoDescription, precioAsiento, residente){
+    var precioResidente = precioAsiento * 0.75;
+    console.log(precioResidente);
+    document.writeln(
+        'Su asiento es el ' + idAsiento 
+        + '<br> en la ' + tipoAsiento + '.<br>' + asientoDescription + '<br>'
+    );
+    if (residente){
+        document.writeln(precioResidente );
+    }else{
+        document.writeln(precioAsiento);
+    }
+}
